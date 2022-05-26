3b9ae9b0dc4093756e6176c976412c825c909487
matthiasmeissen <meissenmatthias@gmail.com>
2022-05-26 16:59:38 +0200

    Completed Playground 1

--- playground-01-220526/script.js
+++ playground-01-220526/script.js
@@ -3,7 +3,7 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({color: 0xffff00})
+const material = new THREE.MeshBasicMaterial({color: 'hsl(160, 100%, 50%)'})
 const mesh = new THREE.Mesh(geometry, material)
 
 scene.add(mesh)
@@ -15,4 +15,12 @@ scene.add(camera)
 const renderer = new THREE.WebGLRenderer({canvas})
 renderer.setSize(window.innerWidth, window.innerHeight)
 
-renderer.render(scene, camera)
+function animate () {
+    requestAnimationFrame(animate)
+
+    mesh.rotation.y += 0.01
+
+    renderer.render(scene, camera)
+}
+
+animate()
