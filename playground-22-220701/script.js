0697579c585760d5161115dfdf0ad242809431da
matthiasmeissen <meissenmatthias@gmail.com>
2022-07-01 18:14:48 +0200

    Added Playground 22

---
+++ playground-22-220701/script.js
@@ -0,0 +1,180 @@
+const canvas = document.querySelector('canvas.webgl')
+
+
+// Scene
+const scene = new THREE.Scene()
+
+
+// Debug
+var GUI = lil.GUI
+const gui = new GUI()
+gui.close(true)
+
+
+/*
+    Geometry
+*/
+
+const group = new THREE.Group()
+
+for (let i = 0; i < 20; i++) {
+    const mesh = new THREE.Mesh(
+        new THREE.BoxGeometry(2, 0.02, 0.4),
+        new THREE.MeshStandardMaterial({
+            emissive: 'hsl(0, 100%, 50%)'
+        }
+        )
+    )
+
+    mesh.position.y = i * 0.04
+    mesh.position.z = i * -0.1
+
+    mesh.rotation.x = i * 0.1
+
+    group.add(mesh)
+}
+
+scene.add(group)
+
+const animateMesh = function () {
+    group.children.forEach((mesh, index) => {
+        mesh.material.color.setHSL(index / group.children.length, 1.0, 0.5)
+        mesh.material.emissive.setHSL(Math.sin(absTime * 0.2), 1.0, 0.5)
+        mesh.scale.x = (index / group.children.length) * cursor.x + 1
+    });
+
+    group.position.z = 1
+    group.rotation.y = absTime * 0.4
+}
+
+/*
+    Lights
+*/
+
+
+// Parameters
+const lightParameters = {
+    rotate: true,
+    speed: 0.2
+}
+
+// Construction
+const lights = new THREE.Group()
+scene.add(lights)
+
+const ambientLight = new THREE.AmbientLight( 0xffffff, 0.2)
+scene.add(ambientLight)
+
+const light1 = new THREE.DirectionalLight(0xffffff, 0.8)
+light1.position.set(2, 2, 4)
+lights.add(light1)
+
+const light2 = new THREE.DirectionalLight(0xffffff, 0.8)
+light2.position.set(-2, 2, -4)
+lights.add(light2)
+
+// Functions
+const rotateLights = () => {
+    lights.rotation.y = absTime * lightParameters.speed
+    lights.rotation.x = absTime * lightParameters.speed
+}
+
+// Debug
+const lightFolder = gui.addFolder('Light')
+lightFolder.add(lightParameters, 'speed').name('Speed').min(0).max(8).step(0.1)
+lightFolder.addColor(light1, 'color').name('Color 1')
+lightFolder.addColor(light2, 'color').name('Color 2')
+
+
+/*
+    Camera
+*/
+
+
+// Construction
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight)
+camera.position.set(0, 0, 4)
+scene.add(camera)
+
+// Functions
+const moveCamera = () => {
+    camera.position.x = Math.sin(cursor.x) * 3
+    camera.position.z = Math.cos(cursor.y) * 3
+    camera.position.y = cursor.y * 2
+
+    camera.lookAt(new THREE.Vector3())
+}
+
+
+/*
+    Renderer
+*/
+
+
+// Construction
+const renderer = new THREE.WebGLRenderer({canvas})
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+// Functions
+window.addEventListener( 'resize', onWindowResize );
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+
+// Fullscreen
+
+window.addEventListener('dblclick', function () {
+
+    const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
+
+    if (!fullscreenElement) {
+        if (canvas.requestFullscreen) {
+            canvas.requestFullscreen()
+        } else if (canvas.webkitRequestFullscreen) {
+            canvas.webkitRequestFullscreen()
+        }
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen()
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen()
+        }
+    }
+})
+
+
+// Mouse Position
+
+const cursor = new THREE.Vector2()
+
+window.addEventListener('mousemove', function (event) {
+    cursor.x = (event.clientX / window.innerWidth) * 2 - 1
+    cursor.y = - (event.clientY / window.innerHeight) * 2 + 1
+})
+
+
+// Clock
+
+const clock = new THREE.Clock()
+let absTime
+
+
+// Animate
+
+function animate () {
+    absTime = clock.getElapsedTime()
+
+    moveCamera()
+    rotateLights()
+    animateMesh()
+
+    renderer.render(scene, camera)
+    requestAnimationFrame(animate)
+}
+
+animate()
