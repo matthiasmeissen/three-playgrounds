115cc2bf8fcc2b7c1d0ecc25cbdcf8782135740e
matthiasmeissen <meissenmatthias@gmail.com>
2022-07-02 23:50:41 +0200

    Added Playground 23

---
+++ playground-23-220702/script.js
@@ -0,0 +1,187 @@
+const canvas = document.querySelector('canvas.webgl')
+
+
+// Scene
+const scene = new THREE.Scene()
+
+
+// Debug
+var GUI = lil.GUI
+const gui = new GUI()
+gui.close(true)
+
+
+/*
+    Geometry
+*/
+
+let vector = new THREE.Vector3(0, 1, 0)
+
+const plane = new THREE.Plane(vector, 0)
+
+const group1 = new THREE.Group()
+
+const num = 20
+
+for (let i = 0; i < num; i++) {
+    const cube = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshStandardMaterial({
+            side: THREE.DoubleSide,
+            clippingPlanes: [plane]
+        })
+    )
+
+    cube.scale.setScalar(1 - (i / num) * 0.6)
+
+    cube.material.color.setHSL(i / num, 1.0, 0.5)
+
+    group1.add(cube)
+}
+
+scene.add(group1)
+
+group1.children[0].material.color.setHSL(0, 0, 0)
+group1.children[0].material.emissive.setHSL(0, 0, 0.04)
+
+
+const rotatePlane = function () {
+    let axis = new THREE.Vector3(1, 0, 1)
+    let angle = 0.02
+
+    vector.applyAxisAngle (axis, angle)
+}
+
+
+/*
+    Lights
+*/
+
+
+// Parameters
+const lightParameters = {
+    rotate: true,
+    speed: 0.2
+}
+
+// Construction
+const lights = new THREE.Group()
+scene.add(lights)
+
+const ambientLight = new THREE.AmbientLight( 0xffffff, 0.2)
+scene.add(ambientLight)
+
+const light1 = new THREE.DirectionalLight(0xffffff, 0.8)
+light1.position.set(2, 2, 4)
+lights.add(light1)
+
+const light2 = new THREE.DirectionalLight(0xffffff, 0.8)
+light2.position.set(-2, 2, -4)
+lights.add(light2)
+
+// Functions
+const rotateLights = () => {
+    lights.rotation.y = absTime * lightParameters.speed
+    lights.rotation.x = absTime * lightParameters.speed
+}
+
+// Debug
+const lightFolder = gui.addFolder('Light')
+lightFolder.add(lightParameters, 'speed').name('Speed').min(0).max(8).step(0.1)
+lightFolder.addColor(light1, 'color').name('Color 1')
+lightFolder.addColor(light2, 'color').name('Color 2')
+
+
+/*
+    Camera
+*/
+
+
+// Construction
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight)
+camera.position.set(0, 0, 4)
+scene.add(camera)
+
+// Functions
+const moveCamera = () => {
+    camera.position.x = Math.sin(cursor.x) * 3
+    camera.position.z = Math.cos(cursor.y) * 3
+    camera.position.y = cursor.y * 2
+
+    camera.lookAt(new THREE.Vector3())
+}
+
+
+/*
+    Renderer
+*/
+
+
+// Construction
+const renderer = new THREE.WebGLRenderer({canvas})
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.localClippingEnabled = true;
+
+// Functions
+window.addEventListener( 'resize', onWindowResize );
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+
+// Fullscreen
+
+window.addEventListener('dblclick', function () {
+
+    const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
+
+    if (!fullscreenElement) {
+        if (canvas.requestFullscreen) {
+            canvas.requestFullscreen()
+        } else if (canvas.webkitRequestFullscreen) {
+            canvas.webkitRequestFullscreen()
+        }
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen()
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen()
+        }
+    }
+})
+
+
+// Mouse Position
+
+const cursor = new THREE.Vector2()
+
+window.addEventListener('mousemove', function (event) {
+    cursor.x = (event.clientX / window.innerWidth) * 2 - 1
+    cursor.y = - (event.clientY / window.innerHeight) * 2 + 1
+})
+
+
+// Clock
+
+const clock = new THREE.Clock()
+let absTime
+
+
+// Animate
+
+function animate () {
+    absTime = clock.getElapsedTime()
+
+    moveCamera()
+    rotateLights()
+    rotatePlane()
+
+    renderer.render(scene, camera)
+    requestAnimationFrame(animate)
+}
+
+animate()
