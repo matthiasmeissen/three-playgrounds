a47e20d93e82bddaa66e904a4a5a5df783b2a374
matthiasmeissen <meissenmatthias@gmail.com>
2022-10-29 23:52:56 +0200

    Completed Playground 56

--- playground-56-221029/script.js
+++ playground-56-221029/script.js
@@ -14,35 +14,77 @@ const scene = new THREE.Scene()
 */
 
 
-const group1 = new THREE.Group()
-group1.scale.setScalar(0.4)
-scene.add(group1)
+const layers = []
 
-const createBox = function (p) {
+
+const createLayer = function () {
+    const group = new THREE.Group()
+    group.scale.setScalar(0.4)
+    scene.add(group)
+
+    layers.push(group)
+}
+
+createLayer()
+
+let currentLayer = 0
+
+
+const setActiveLayer = function (n) {
+    layers.forEach(layer => {
+        layer.visible = false
+    })
+
+    layers[n].visible = true
+}
+
+let allowPlay = false
+
+
+setInterval(function () {
+    if (allowPlay) {
+        currentLayer += 1
+
+        if (currentLayer > layers.length - 1) {
+            currentLayer = 0
+        }
+
+        setActiveLayer(currentLayer)
+    }
+}, 200)
+
+
+
+const createBox = function (p, layer) {
     const box = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
         new THREE.MeshStandardMaterial({color: 'hsl(0, 0%, 20%)'})
     )
     box.position.set(p.x, p.y, p.z)
-    group1.add(box)
+    layers[layer].add(box)
 }
 
-createBox({x: 0, y: 0, z: 0})
 
+const makePreviewCube = function () {
+    const group = new THREE.Group()
+    group.scale.setScalar(0.4)
+    scene.add(group)
 
+    const previewCube = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshStandardMaterial({
+            color: 'hsl(0, 0%, 80%)',
+            transparent: true,
+            opacity: 0.4
+        })
+    )
 
+    group.add(previewCube)
 
-const group2 = new THREE.Group()
-group2.scale.setScalar(0.4)
-scene.add(group2)
+    return previewCube
+}
 
-const previewCube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshStandardMaterial({color: 'hsl(0, 0%, 80%)'})
-)
-
-group2.add(previewCube)
-
+const previewCube = makePreviewCube()
 
 
 const setMeshPosition = function (target, direction) {
@@ -58,28 +100,17 @@ const setMeshPosition = function (target, direction) {
 setMeshPosition(previewCube, {x: 0, y: 1, z: 0})
 
 
-const viewDirection = function () {
-    const d = camera.getWorldDirection(new THREE.Vector3())
-
-    let direction = ''
-
-    if (d.z < -0.5) {
-        direction = 'Front'
-    } else if (d.z > 0.5) {
-        direction = 'Back'
-    } else if (d.x < -0.5) {
-        direction = 'Right'
-    } else if (d.x > 0.5) {
-        direction = 'Left'
-    } else if (d.y < -0.5) {
-        direction = 'Top'
-    } else if (d.y > 0.5) {
-        direction = 'Bottom'
-    }
-
-    return direction
+const checkForCubes = function (n) {
+    const p = previewCube.position
+    layers[n].children.forEach(target => {
+        const t = target.position
+        if (t.x == p.x && t.y == p.y && t.z == p.z) {
+            target.geometry.dispose()
+            target.material.dispose()
+            layers[n].remove(target)
+        }
+    });
 }
-
 
 
 
@@ -171,48 +202,56 @@ window.addEventListener('mousemove', function (event) {
 
 window.addEventListener('keydown', function (n) {
 
-    const v = viewDirection()
-
     if (n.key == 'ArrowUp') {
-        if (v == 'Front' || v == 'Back' || v == 'Right' || v == 'Left') {
-            setMeshPosition(previewCube, {x: 0, y: 1, z: 0})
-        } else if (v == 'Top') {
-            setMeshPosition(previewCube, {x: 0, y: 0, z: -1})
-        } else if (v == 'Bottom') {
-            setMeshPosition(previewCube, {x: 0, y: 0, z: 1})
+        if (n.altKey == true) {
+            createLayer()
+            currentLayer = layers.length - 1
+            setActiveLayer(currentLayer)
+            return
         }
+
+        setMeshPosition(previewCube, {x: 0, y: 1, z: 0})
+
     } else if (n.key == 'ArrowDown') {
-        if (v == 'Front' || v == 'Back' || v == 'Right' || v == 'Left') {
-            setMeshPosition(previewCube, {x: 0, y: -1, z: 0})
-        } else if (v == 'Top') {
-            setMeshPosition(previewCube, {x: 0, y: 0, z: 1})
-        } else if (v == 'Bottom') {
-            setMeshPosition(previewCube, {x: 0, y: 0, z: -1})
-        }
+        setMeshPosition(previewCube, {x: 0, y: -1, z: 0})
+
     } else if (n.key == 'ArrowRight') {
-        if (v == 'Front' || v == 'Top') {
-            setMeshPosition(previewCube, {x: 1, y: 0, z: 0})
-        } else if (v == 'Back') {
-            setMeshPosition(previewCube, {x: -1, y: 0, z: 0})
-        } else if (v == 'Left' || v == 'Bottom') {
-            setMeshPosition(previewCube, {x: 0, y: 0, z: 1})
-        } else if (v == 'Right') {
-            setMeshPosition(previewCube, {x: 0, y: 0, z: -1})
+        if (n.altKey == true) {
+            currentLayer += 1
+            if (currentLayer > layers.length - 1) {
+                currentLayer = 0
+            }
+            setActiveLayer(currentLayer)
+            return
         }
+
+        setMeshPosition(previewCube, {x: 1, y: 0, z: 0})
+
     } else if (n.key == 'ArrowLeft') {
-        if (v == 'Front' || v == 'Top') {
-            setMeshPosition(previewCube, {x: -1, y: 0, z: 0})
-        } else if (v == 'Back') {
-            setMeshPosition(previewCube, {x: 1, y: 0, z: 0})
-        } else if (v == 'Left' || v == 'Bottom') {
-            setMeshPosition(previewCube, {x: 0, y: 0, z: -1})
-        } else if (v == 'Right') {
-            setMeshPosition(previewCube, {x: 0, y: 0, z: 1})
+        if (n.altKey == true) {
+            currentLayer -= 1
+            if (currentLayer < 0) {
+                currentLayer = layers.length - 1
+            }
+            setActiveLayer(currentLayer)
+            return
         }
+
+        setMeshPosition(previewCube, {x: -1, y: 0, z: 0})
+    }
+
+    if (n.key == 'p') {
+        allowPlay = !allowPlay
+
+        previewCube.visible = !allowPlay
+    }
+
+    if (n.key == 'd') {
+        checkForCubes(currentLayer)
     }
 
     if (n.code == 'Space') {
-        createBox(previewCube.position)
+        createBox(previewCube.position, currentLayer)
     }
 })
 
