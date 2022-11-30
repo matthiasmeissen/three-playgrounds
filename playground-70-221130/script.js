83622a5128999c3cd519c1c4b90fbba7ef41eb08
matthiasmeissen <meissenmatthias@gmail.com>
2022-11-30 23:01:28 +0100

    Completed Playground 70

--- playground-70-221130/script.js
+++ playground-70-221130/script.js
@@ -36,9 +36,12 @@ class Structure {
 
     setSize(s = 2, h = 0.1) {
         this.group.children.forEach((mesh, index) => {
-            mesh.scale.x = s - (index / this.num) * s
+            mesh.scale.x = s - (index / this.num) * s * 0.2
             mesh.scale.y = h
             mesh.scale.z = s - (index / this.num) * s
+
+            if (index%2 == 1) {mesh.scale.x = mesh.scale.x * 1.2}
+            if (index%4 == 1) {mesh.scale.setScalar(0)}
 
             mesh.position.y = index * h * 1.1
         })
@@ -54,17 +57,51 @@ class Structure {
 
     changeColor(t) {
         this.group.children.forEach((mesh, index) => {
-            const d = Math.sin(index * 0.5 + t)
+            const d = Math.sin(index * 0.8 + t)
             mesh.material.color.setHSL(index * 0.02 + 0.4, d, 0.4)
         });
     }
 }
 
 const structure1 = new Structure()
-
 structure1.setSize()
 
 
+class Box {
+    constructor(num) {
+        this.num = num
+        this.group = new THREE.Group()
+
+        this.createBox()
+    }
+
+    createBox() {
+        for (let i = 0; i < this.num; i++) {    
+            const box = new THREE.Mesh(
+                new THREE.BoxGeometry(),
+                new THREE.MeshStandardMaterial()
+            )
+            this.group.add(box)
+        }
+
+        scene.add(this.group)
+    }
+
+    setSize() {
+        this.group.children.forEach(mesh => {
+            mesh.scale.set(Math.random() * 0.2, Math.random() * 0.2, Math.random() * 0.2)
+            mesh.rotation.set(Math.random() * Math.PI * 2.0, Math.random() * Math.PI * 2.0, Math.random() * Math.PI * 2.0)
+            mesh.position.set((Math.random() - 0.5) * 4, (Math.random() - 0.5) * 4, (Math.random() - 0.5) * 4)
+        });
+
+    }
+}
+
+const box1 = new Box(20)
+
+setInterval(function() {
+    box1.setSize()
+}, 400)
 
 
 
@@ -177,7 +214,7 @@ let absTime
 function animate() {
     absTime = clock.getElapsedTime()
 
-    structure1.rotate(absTime)
+    structure1.rotate(absTime * 0.4)
     structure1.changeColor(absTime)
 
     renderer.render(scene, camera)
